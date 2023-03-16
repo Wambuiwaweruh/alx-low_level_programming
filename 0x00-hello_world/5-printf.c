32691aa42f32caa85a64b54c571c4ad4fdb4c405
Wambuiwaweruh <wambuiwaweruh@gmail.com>
2023-03-16 13:46:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- *main-prints a c program using printf
+ * main - print a c program using printf
  *
- *return:0
+ * Return: 0
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
