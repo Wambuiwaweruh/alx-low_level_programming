ad65f00ddd7aeccbb7fd6fcf91948fd862790b49
Wambuiwaweruh <wambuiwaweruh@gmail.com>
2023-03-22 01:39:39 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Addition calculate
+ * @num1: first int
+ * @num2: second
+ * Return: Addition solution
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
