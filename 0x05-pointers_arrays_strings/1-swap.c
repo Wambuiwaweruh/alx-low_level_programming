6930d9d9ee61675ebd6cebd74dc4bf4179e41abc
Wambuiwaweruh <wambuiwaweruh@gmail.com>
2023-03-27 07:57:15 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of int a and int b
+* Owned by Bwave/Bright
+* @a: first int
+* @b: second int
+* Return 0
+*/
+void swap_int(int *a, int *b)
+{
+
+	int r;
+
+	r = *a;
+	*a = *b;
+	*b = r;
+}
+
