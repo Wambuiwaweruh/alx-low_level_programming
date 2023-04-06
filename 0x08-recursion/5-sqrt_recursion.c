dd5ccf1ef7c2de98f900ac45d0e6fd37cab718e0
Wambuiwaweruh <wambuiwaweruh@gmail.com>
2023-04-05 21:14:37 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+int sqrt_scanner(int n, int freq);
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * sqrt_scanner - sqaure root
+ * @n:interger
+ * Return:interger
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_scanner(n, 0));
+}
+
+/**
+ * sqrt_scanner - square root
+ * @n:interger
+ * @b:checks number of times squareroot is repeated
+ * Return:interger
+ */
+int sqrt_scanner(int n, int b)
+{
+	if (b * b > n)
+		return (-1);
+	if (b * b == n)
+		return (b);
+	return (sqrt_scanner(n, b + 1));
+}
