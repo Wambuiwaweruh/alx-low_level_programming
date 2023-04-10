4d9c021f75093ee7023cbba18a1f34afccbfb91b
Wambuiwaweruh <wambuiwaweruh@gmail.com>
2023-04-10 11:02:02 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that adds positive numbers.
+ * @argc: no of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int a, n, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (a = 1; argv[a]; a++)
+	{
+		n = strtol(argv[a], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
