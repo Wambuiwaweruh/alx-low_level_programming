7907329b294bb6102548d0d31e54b4190cddb024
Wambuiwaweruh <wambuiwaweruh@gmail.com>
2023-04-11 12:13:28 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * str_concat -  function that concatenates two strings
+ * str_concat - a function that concatenates two strings.
  * @s1:first string
  * @s2:second string
- * Return: Null on failure, else return new address
+ * Return:NULL on failure, else new adress
  */
 char *str_concat(char *s1, char *s2)
 {
-	int a_s1, b_s2, length_s1, length_s2, length;
+	int a, b, length_s1, length_s2, length;
 	char *product;
 
-	length_s1 = 0;
-	length_s2 = 0;
+	length_s1 = length_s2 = 0;
 
 	if (s1 != NULL)
 	{
-		a_s1 = 0;
-		while (s1[a_s1++] != '\0')
+		a = 0;
+		while (s1[a++] != '\0')
 			length_s1++;
 	}
 
 	if (s2 != NULL)
 	{
-		a_s1 = 0;
-		while (s2[a_s1++] != '\0')
+		a = 0;
+		while (s2[a++] != '\0')
 			length_s2++;
 	}
 
 	length = length_s1 + length_s2;
-	product = malloc(sizeof(char) * (length + 1));
+	product = (char *)malloc(sizeof(char) * (length + 1));
 	if (product == NULL)
 		return (NULL);
 
-	for (a_s1 = 0; a_s1 < length_s1; a_s1++)
-		product[a_s1] = s1[a_s1];
-	for (b_s2 = 0; b_s2 < length_s2; b_s2++)
-		product[a_s1] = s2[b_s2];
+	for (a = 0; a < length_s1; a++)
+		product[a] = s1[a];
+	for (b = 0; b < length_s2; b++, a++)
+		product[a] = s2[b];
 	product[length] = '\0';
 
 	return (product);
