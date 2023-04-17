26ac05072348aded716f1e4fd227b877a6c276f0
Wambuiwaweruh <wambuiwaweruh@gmail.com>
2023-04-17 09:55:40 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,27 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG
+#define DOG
 
 /**
- * struct dog - function that contains a dogs description
- * @name:the name of dog
- * @age:the age of the dog
- * @owner:owner of the dog
+ * struct dog - description of the dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  */
+
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
 
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
